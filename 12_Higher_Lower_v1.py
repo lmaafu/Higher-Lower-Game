87a80ed11105a43bb9be2d80ce9bf3db69915483
Lavinia Ma'afu <>
2021-04-15 01:13:32 +0000

    partially working base

--- 12_Higher_Lower_v1.py
+++ 12_Higher_Lower_v1.py
@@ -1,3 +1,7 @@
+import random
+
+import math
+
 # HL  component 1 - Get (and check) user input 
 
 # To do 
@@ -9,6 +13,8 @@
 
 
 # Number checking function goes here 
+# checks for an integer, optionally checks 
+# for a minimum / number between two values
 def int_check(question, low=None, high=None): 
 
   situation = ""
@@ -45,63 +51,78 @@ def int_check(question, low=None, high=None):
       continue
 
 
-
-
-# Main routine 
-
-lowest = int_check("Low Number: ")
-highest = int_check(" High Number: ", lowest + 1)
-rounds = int_check( "Rounds: ", 1)
-guess = int_check("Guess: ", lowest, highest)
-
-# HL component 5 - no duplicates
-
-# To Do 
-# set up empty list called already_guessed
-# when user guesses, add guess to list 
-# for each guess, check that number is not in already_guessed
-
-# HL component 5 - Prevents duplicate gusses
-
-SECRET = 7
-GUESSES_ALLOWED = guess
-
-already_guessed =[]
-guesses_left = GUESSES_ALLOWED
-num_won = 0
-
-guess = ""
-
-while guess != SECRET and guesses_left >= 1:
-  guess = int(input("Guesses: "))  # replace this with function
-
-  #v checks that guess is not a duplicate 
-  if guess in already_guessed :
-    print("You already guessed that number! Please try again ","You *still* have {} guesses left".format(guesses_left, ))
-    continue
   
-  guesses_left -= 1
-  already_guessed.append(guess)
+# Main routine 
+# HL component 11 - Maximum Guesses Calculator
 
-  if guesses_left >= 1:
 
-    if guess < SECRET:
-      print("Too low, try a higher number. Guesses left : {}".format(guesses_left))
+for item in range(0, 4):   # loop component for esay testing......
 
-    elif guess > SECRET:
-      print("Too high, try a lower number. Guesses left : {}".format(guesses_left))
+  low = int(input("Low: "))   # use int check in due course 
+  high = int(input("High: ")) # use int check in due course 
 
-  else:
-    if guess < SECRET:
-      print("Too low!")
-    elif guess > SECRET:
-      print("Too high!")
+  range = high - low + 1
+  max_raw = math.log2(range)  # finds maximum # of guesses using math.log2
+  max_upped = math.ceil(max_raw)  # rounds up (ceil----> ceiling )
+  max_guesses = max_upped + 1
+  print("Max Guessed: {}".format(max_guesses))
 
-if guess == SECRET:
-  if guesses_left == GUESSES_ALLOWED -1:
-   print("Amazing! You got it")
-else:
-  print("Well done! You got it")
+  print()
+
+  lowest = int_check("Low Number: ")
+  highest = int_check(" High Number: ", lowest + 1)
+  rounds = int_check( "Rounds: ", 1)
+  guess = int_check("Guess: ", lowest, highest)
+
+  # HL component 5 - no duplicates
+
+  # To Do 
+  # set up empty list called already_guessed
+  # when user guesses, add guess to list 
+  # for each guess, check that number is not in already_guessed
+
+  # HL component 5 - Prevents duplicate gusses
+
+  secret = 7
+  guesses_allowed = guess 
+
+  already_guessed =[]
+  guesses_left = guesses_allowed
+  num_won = 0
+
+  guess = ""
+
+  while guess != secret and guesses_left >= 1:
+    guess = int(input("Guesses: "))  # replace this with function
+
+    #v checks that guess is not a duplicate 
+    if guess in already_guessed :
+      print("You already guessed that number! Please try again ","You *still* have {} guesses left".format(guesses_left))
+      continue
+    
+    guesses_left -= 1
+    already_guessed.append(guess)
+
+    if guesses_left >= 1:
+
+      if guess < secret:
+        print("Too low, try a higher number. Guesses left : {}".format(guesses_left))
+
+      elif guess > secret:
+        print("Too high, try a lower number. Guesses left : {}".format(guesses_left))
+      continue
+      
+    else:
+      if guess < secret:
+        print("Too low!")
+      elif guess > secret:
+        print("Too high!")
+
+    if guess == secret:
+      if guesses_left == guesses_allowed -1:
+        print("Amazing! You got it")
+    else:
+      print("Well done! You got it")
   
   # HL component 11 - Maximum Guesses Calculator
 
