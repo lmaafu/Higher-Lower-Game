77f63eb3480ec91d42460e33260ad0a94c4266ab
Lavinia Ma'afu <>
2021-05-03 21:54:55 +0000

    adding in rounds

--- 06_HL_Statement_Generator.py
+++ 06_HL_Statement_Generator.py
@@ -1,26 +1,18 @@
-# function go here 
-def instructions ():
-  print("**** Welcome to the Higher or Lower Game ****")
-  print("For each game you will be asked to....")
-  print("- Enter a 'low' and 'high' number. The compute'r will randomly generate a 'secret' number between your two chosen numbers. It will use these numbers for all the rounds in agiven game.")
-  print(" - The computer will calculate how many guesses yo are allowed ")
-  print("- enter the onumber of rounds you want to play")
-  print("- guess the secret number")
-  print()
-  print()
-  print("Good Luck!")
-  
-def yes_no(question):
-    valid = False
-    while not valid:
-        response = input(question).lower()
-        if response == "yes" or response == "y":
-            response = "yes"
-            return response
+def statement_generator(statement, decoration):
 
-        elif response == "no" or response == "n":
-            response = "no"
-            return response
+    sides = decoration * 3
 
-        else:
-            print("Please answer yes/no")
+    statement = "{} {} {}".format(sides, statement, sides)
+    top_bottom = decoration * len(statement)
+
+    print(top_bottom)
+    print(statement)
+    print(top_bottom)
+
+    return ""
+
+
+# Main routine goe here
+statement_generator("Welcome to Higher or Lower", "*")
+print()
+statement_generator("Congratulations you got  it ", "*")
--- 12_Higher_Lower_v1.py
+++ 12_Higher_Lower_v1.py
@@ -1,10 +1,11 @@
-import random
 import math
+import random
 
 # HL  component 1 - Get (and check) user input 
 # Function goes here 
+# displays instructins for the user 
 def instructions ():
-  print("**** Welcome to the Higher or Lower Game ****")
+  statement_generator(" Welcome to the Higher or Lower Game ", "*")
   print("For each game you will be asked to....")
   print("- Enter a 'low' and 'high' number. The compute'r will randomly generate a 'secret' number between your two chosen numbers. It will use these numbers for all the rounds in agiven game.")
   print(" - The computer will calculate how many guesses yo are allowed ")
@@ -13,7 +14,7 @@ def instructions ():
   print()
   print()
   print("Good Luck!")
-  
+# Checks if the use asnwers yes or no for othe given queston 
 def yes_no(question):
     valid = False
     while not valid:
@@ -28,29 +29,20 @@ def yes_no(question):
 
         else:
             print("Please answer yes/no")
+# Decorations for the sides of the heading 
+def statement_generator(statement, decoration):
 
+    sides = decoration * 3
 
+    statement = "{} {} {}".format(sides, statement, sides)
+    top_bottom = decoration * len(statement)
 
-# Main  routine
-played_before= yes_no("Have you played the game before? ")
-print()
+    print(top_bottom)
+    print(statement)
+    print(top_bottom)
 
-if played_before == "no":
-    instructions()
-
-print()
-
-# To do 
-# Check a low is an integer (any integer)
-# Check that high is more than low (lower bound only)
-# Check that rounds is more than 1 (upper bound only)
-# Check that guss is betweeen low and high 9
-# lower and upper bound0
-
-
-# Number checking function goes here 
-# checks for an integer, optionally checks 
-# for a minimum / number between two values
+    return ""
+# integer Checker 
 def int_check(question, low=None, high=None): 
 
   situation = ""
@@ -85,15 +77,63 @@ def int_check(question, low=None, high=None):
     except ValueError:
       print("Please enter an integer")
       continue
+# Checks for the number of rounds in the game
+def check_rounds():
+  while True: 
+    print()
+    response = input("How many rounds?: ")
 
+    rounds_error = "Please type either <enter> or an integer that is more than 0"
+    if response != "":
+      try:
+        response = int(response)
 
+        if response <1:
+          print(rounds_error)
+          continue 
+
+        else:
+          return response
+
+      except ValueError:
+        print(rounds_error)
+        continue
+
+    return response 
+# Checks if the answers that the user gives is an integer 
+def num_check(question, low, high):
+    error = " Please enter an whole number between 1 and 10\n "
+
+    valid = False
+    while not valid:
+        try:
+            #   ask the question
+            response = int(input(question))
+
+            # if the amount is too low ? too high give
+            if low < response <= high:
+                return response
+
+# output an error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
   
 # Main routine 
-# HL component 11 - Maximum Guesses Calculator
+played_before= yes_no("Have you played the game before? ")
 print()
-# Set up ythe gam parameters (range, number of numbers)
+
+if played_before == "no":
+    instructions()
+    print()
+# Set up the gam parameters (range, number of numbers)
 low = int(input("Low Number: "))
-high = int(input(" High Number: ", low + 1))
+print()
+high = int(input(" High Number: "))
+print()
+rounds = int(input(" Rounds: "))
 
 # works out nmber of guesses 
 num_range = high - low + 1
@@ -101,12 +141,15 @@ max_raw = math.log2(num_range)  # finds maximum # of guesses using math.log2
 max_upped = math.ceil(max_raw)  # rounds up (ceil----> ceiling )
 max_guesses = max_upped + 1
 
-print("Max Guesses: {}".format(max_guesses )) 
-
-rounds = 2    # replace with call to number checker
-#H/L Integer 
+print("Max Guesses: {}".format(max_guesses ))
+print()
+# Rounds
+rounds_lost = 0
+rounds_won = 0  
+rounds_played = rounds_won + rounds_lost 
 
 for item in range(0, rounds):
+
   secret = random.randit(low, high)
   print("Spoiler alert, the secret is", secret)
   guesses_allowed = max_guesses 
